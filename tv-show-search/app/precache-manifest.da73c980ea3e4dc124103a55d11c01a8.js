c78c54810f4e1a20df77c3238ecfb003c34bb01f
Riccardo Trocca <rtrocca@gmail.com>
2020-01-01 10:20:27 +0100

    commit first app

---
+++ tv-show-search/app/precache-manifest.da73c980ea3e4dc124103a55d11c01a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eb168c41ca1a990fb19a6e65936aa24",
+    "url": "/tv-show-search/app/index.html"
+  },
+  {
+    "revision": "e6cc7455d4a1e3ab114d",
+    "url": "/tv-show-search/app/static/css/main.0d9ca0ea.chunk.css"
+  },
+  {
+    "revision": "cddeefdbcad276a474de",
+    "url": "/tv-show-search/app/static/js/2.f9a21053.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tv-show-search/app/static/js/2.f9a21053.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6cc7455d4a1e3ab114d",
+    "url": "/tv-show-search/app/static/js/main.574d3dfd.chunk.js"
+  },
+  {
+    "revision": "a0990556086ef3730469",
+    "url": "/tv-show-search/app/static/js/runtime-main.6ced307f.js"
+  }
+]);
